442923781f6774ffa473afdaf8aaf002aa6d2a8c
ddS08 <134380878+Dhyaan-tmedia@users.noreply.github.com>
2023-05-29 17:16:22 +0530

    17:16

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useState,useEffect} from 'react';
 import Todo from "./Todo";
 
 import {db} from './firebase';
-import {query,collection, onSnapshot,doc,updateDoc,addDoc  } from 'firebase/firestore';
+import {query,collection, onSnapshot,doc,updateDoc,addDoc,deleteDoc  } from 'firebase/firestore';
 const style={
   bg:`h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
   container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
@@ -49,6 +49,12 @@ function App() {
       })
     }
     //delete todo
+    const deleteTodo = async(id) =>{
+      await deleteDoc(doc(db,'todos',id))
+    }
+
+
+
     return(
     <div className={style.bg}>
       <div className={style.container}>
@@ -59,11 +65,12 @@ function App() {
         </form>
         <ul>
           {todos.map((todo,index)=>(
-            <Todo key={index} todo={todo} toggleComplete={toggleComplete}/>
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
             ))}
           
         </ul>
-        <p className={style.count}> You have 2 Todos</p>
+        {todos.length<1 ? null : <p className={style.count}> {`You have ${todos.length} Todos`}</p>}
+        
       </div>
 
     </div>
